9120f079b21b0dcbbbf8812f633f811ed6bd97bd
epw1624 <112768709+epw1624@users.noreply.github.com>
2023-08-15 13:47:08 -0700

    add home zone setting

--- commands.py
+++ commands.py
@@ -2,6 +2,7 @@
 
 from get_forecast import get_current_forecast, build_forecast
 import discord
+from bot import Bot
 
 client = discord.Client(intents=discord.Intents(messages=True,message_content=True, emojis=True, emojis_and_stickers=True))
 
@@ -9,7 +10,7 @@ EMOJIS = {"low": '�', "moderate": '�',
           "considerable": '�', "high": '�', 
           "extreme": '⬛', "norating": '❔'}
 
-def forecast(lat, long):
+def forecast(lat, long, bot):
     """
     Takes the latitude and longitude from a forecast command and returns a Forecast object
     """
@@ -24,6 +25,25 @@ def forecast(lat, long):
     )
 
     return bot_message
+
+def set_home(lat, long, bot):
+    """
+    Sets the home zone of the bot to be the coordinates (lat, long)
+    """
+    bot.set_home(lat, long)
+    bot_message = "Home coordinates set to:\n Latitude: {lat}\n Longitude: {long}".format(lat=lat, long=long)
+    return bot_message
+
+def home(bot):
+    """
+    Retrieves a forecast for the set home zone of the bot
+    """
+    lat = bot.get_lat()
+    long = bot.get_long()
+
+    return forecast(lat, long, bot)
+
+    
     
     
 
